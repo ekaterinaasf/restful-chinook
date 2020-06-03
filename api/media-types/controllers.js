6055907199671095c4c78bd20edba153097bf447
Ekaterina <nataveres@gmail.com>
2020-06-03 17:39:57 +0200

    Medua types finished

--- api/media-types/controllers.js
+++ api/media-types/controllers.js
@@ -1,27 +1,70 @@
-const db = require('../db-connection');
+const db = require("../db-connection");
 
 const controllers = {
   getAll: (req, res) => {
-
     const sql = `SELECT * FROM media_types`;
 
     db.all(sql, (err, rows) => {
       if (err) {
-        res.status(400).json({ "error": err.message });
+        res.status(400).json({ error: err.message });
         return;
       }
 
-      res.json(rows)
+      res.json(rows);
     });
   },
-  getOne: (req, res) => { },
+  getOne: (req, res) => {
+    const id = Number(req.params.id);
+    let sql = `SELECT * FROM media_types WHERE MediaTypeId=${id}`;
+    db.all(sql, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        console.log(err.message);
+        return;
+      }
+      res.json(row);
+    });
+  },
   create: (req, res) => {
     // read row data from body
+    const name = req.body.name; //Name
+    db.get(`SELECT max(MediaTypeId) AS res FROM media_types`, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        console.log(err.message);
+        return;
+      }
+      console.log(row);
+      let sql = `INSERT INTO media_types(MediaTypeId, Name) VALUES(${
+        row.res + 1
+      }, "${name}")`;
+      console.log(sql);
+      db.run(sql, (err, row) => {
+        if (err) {
+          res.status(400).json({ error: err.message });
+          console.log(err.message);
+          return;
+        }
+        res.json(row);
+      });
+    });
   },
   update: (req, res) => {
     // read row data from body
+    const id = req.params.id;
+    console.log(id);
+    const value = req.body.value; //{"value": "media_for_update"}
+    console.log(value);
+    let sql = `UPDATE media_types SET Name='${value}' WHERE MediaTypeId=${id}`;
+    db.run(sql, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json(row);
+    });
   },
-  delete: (req, res) => { }
-}
+  delete: (req, res) => {},
+};
 
 module.exports = controllers;
